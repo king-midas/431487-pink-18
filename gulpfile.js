97e1e1956a1e178bb14ede5639fa493aab56d25d
king-midas <1nformer294@gmail.com>
2020-01-24 23:38:14 +0300

    Настройка основных файлов для автоматизации сборки - gulpfile.js, package.json, package-lock.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
+var server = require("browser-sync").create();
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
@@ -14,7 +15,6 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
-var server = require("browser-sync").create();
 var uglify = require("gulp-uglify");
 
 gulp.task("css", function () {
@@ -38,80 +38,80 @@ gulp.task("html", function () {
     include()
   ]))
   .pipe(gulp.dest("build"));
-});
+  });
 
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,scg}")
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
   .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true}),
-    imagemin.svgo()
+  imagemin.optipng({optimizationLevel: 3}),
+  imagemin.jpegtran({progressive: true}),
+  imagemin.svgo()
   ]))
   .pipe(gulp.dest("build/img"));
-});
+  });
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
   .pipe(webp({quality: 90}))
   .pipe(gulp.dest("build/img/webp"));
-});
+  });
 
 gulp.task("sprite", function () {
   return gulp.src("source/img/icon-*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
+  .pipe(svgstore({
+    inlineSvg: true
     }))
-  .pipe(rename("sprite.svg"))
-  .pipe(gulp.dest("build/img"));
-});
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+  });
 
-gulp.task("js", function () {
-  return gulp.src("source/js/*.js")
-    .pipe(uglify())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest("build/js"));
- });
+  gulp.task("js", function () {
+    return gulp.src("source/js/*.js")
+      .pipe(uglify())
+      .pipe(rename({suffix: ".min"}))
+      .pipe(gulp.dest("build/js"));
+   });
 
-gulp.task("copy", function () {
-  return gulp.src([
+  gulp.task("copy", function () {
+    return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
+    "source/img/*.{png,jpg,svg}",
     "source/*.ico"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
-});
+    ], {
+    base: "source",
+    ignore:"source/img/icon-*.svg"
+    })
+    .pipe(gulp.dest("build"));
+   });
 
-gulp.task("clean", function () {
-  return del("build");
-});
+   gulp.task("clean", function () {
+    return del("build");
+   });
 
-gulp.task("server", function () {
-  server.init({
-    server: "build/",
-  });
+   gulp.task("server", function () {
+    server.init({
+    server: "build/"
+    });
 
-  gulp.watch("source/js/*.js", gulp.series("js"));
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
-});
+    gulp.watch("source/js/*.js", gulp.series("js"));
+    gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+    gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+    gulp.watch("source/*.html", gulp.series("html", "refresh"));
+    });
 
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
-  });
+    gulp.task("refresh", function (done) {
+    server.reload();
+    done();
+    });
 
-gulp.task("build", gulp.series(
-  "clean",
-  "copy",
-  "webp",
-  "js",
-  "css",
-  "sprite",
-  "html"
-));
+    gulp.task("build", gulp.series(
+      "clean",
+      "copy",
+      "webp",
+      "js",
+      "css",
+      "sprite",
+      "html"
+    ));
 
-gulp.task("start", gulp.series("css", "server"));
+    gulp.task("start", gulp.series("build", "server"));
